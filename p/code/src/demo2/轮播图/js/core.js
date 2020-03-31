42b75fa85b073dd0ec7a954347f1341e874055c8
asuperdog <58942095+asuperdog@users.noreply.github.com>
2020-03-31 11:28:05 +0800

    renew

--- p/code/src/demo2/轮播图/js/core.js
+++ p/code/src/demo2/轮播图/js/core.js
@@ -1,5 +1,5 @@
 var index = 0;
-var img = ["url('img/01.png')", "url('img/02.png')", "url('img/03.png')", "url('img/04.png')"];
+var img = ["url('https://i.loli.net/2020/03/31/ZemNH3jxInp5fu9.png')", "url('https://i.loli.net/2020/03/31/mO53zFghquC4dWn.png')", "url('https://i.loli.net/2020/03/31/LTDaFtReJ7I4vV6.png')", "url('https://i.loli.net/2020/03/31/cOvz867KQEXBVGq.png')"];
 
 var timer = setInterval(function () {
     var bg = document.getElementById("container");
@@ -17,25 +17,25 @@ var img2 = document.getElementById("li2");
 var img3 = document.getElementById("li3");
 var img4 = document.getElementById("li4");
 
-img1.onmousemove = function() {
+img1.onmousemove = function () {
     clearInterval(timer)
     img1.style.borderColor = "orange"
     img1.style.backgroundColor = "orange"
 }
 
-img2.onmousemove = function() {
+img2.onmousemove = function () {
     clearInterval(timer)
     img2.style.borderColor = "orange"
     img2.style.backgroundColor = "orange"
 }
 
-img3.onmousemove = function() {
+img3.onmousemove = function () {
     clearInterval(timer)
     img3.style.borderColor = "orange"
     img3.style.backgroundColor = "orange"
 }
 
-img4.onmousemove = function() {
+img4.onmousemove = function () {
     clearInterval(timer)
     img4.style.borderColor = "orange"
     img4.style.backgroundColor = "orange"
