ae62f051c13c1750580496eba868fe93acfc5ad5
asuperdog <58942095+asuperdog@users.noreply.github.com>
2020-08-16 12:20:16 +0800

    final

---
+++ p/about/js/playlist.js
@@ -0,0 +1,6 @@
+var playlist = [
+    { title: "小宇", artist: "Todd Li", mp3: "https://superdog.oss-cn-hongkong.aliyuncs.com/music/001.mp3", cover: "https://p1.music.126.net/4azOvzVWCzln6E84lPKZ9A==/109951164548112691.jpg", },
+    { title: "飘洋过海来看你", artist: "梁静茹,艾怡良", mp3: "https://superdog.oss-cn-hongkong.aliyuncs.com/music/002.mp3", cover: "https://p2.music.126.net/W7wzQYO3zd5qW3ZejF9vSA==/109951164952946901.jpg", },
+];
+var isRotate = true;
+var autoplay = false;
